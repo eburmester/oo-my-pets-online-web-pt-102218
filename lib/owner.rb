be828379d1c429e810a5b719ef335611bae37338
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-27 17:56:00 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,9 +29,16 @@ class Owner
   
   
   def buy_fish(name_of_fish)
-    @pets[:fishes] << 
+    @pets[:fishes] << Fish.new(name_of_fish)
   end
   
+  def buy_cat(name_of_fish)
+    @pets[:fishes] << Fish.new(name_of_fish)
+  end
+  
+  def buy_fish(name_of_fish)
+    @pets[:fishes] << Fish.new(name_of_fish)
+  end
     
   
 end
